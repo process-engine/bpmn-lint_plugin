c907b1c1d845da846247b77e7f0e98f7591d0b2c
Vyperus <bastian.schnittger@5minds.de>
2019-02-20 16:32:41 +0100

    :sparkles: Add Start Event Required Rule

---
+++ src/start-event-required.ts
@@ -0,0 +1,49 @@
+import {IModdleElement, IProcessRef} from '@process-engine/bpmn-elements_contracts';
+
+import * as lintUtils from 'bpmnlint-utils';
+import {BpmnLintReporter} from './contracts/bpmn-lint-reporter';
+
+/**
+ * Rule that reports if a process has no StartEvent.
+ */
+module.exports = (): any => {
+
+  function hasNoStartEvent(node: IModdleElement): boolean {
+    const flowElements: Array<IModdleElement> = node.flowElements;
+
+    const startEventPresent: boolean = flowElements.some((element: IModdleElement) => {
+      return lintUtils.is(element, 'bpmn:StartEvent');
+    });
+
+    return !startEventPresent;
+  }
+
+  function getParticipantOfProcess(process: IProcessRef): IModdleElement {
+    const collaboration: IModdleElement = process.$parent.rootElements.find((element: IModdleElement) => {
+      return lintUtils.is(element, 'bpmn:Collaboration');
+    });
+
+    const participant: IModdleElement = collaboration.participants[0];
+
+    return participant;
+  }
+
+  function check(node: IModdleElement, reporter: BpmnLintReporter): void {
+    const nodeIsProcessOrSubProcess: boolean = lintUtils.isAny(node, ['bpmn:Process', 'bpmn:SubProcess']);
+
+    if (nodeIsProcessOrSubProcess) {
+      const processHasNoStartEvent: boolean = hasNoStartEvent(node);
+
+      if (processHasNoStartEvent) {
+        const participant: IModdleElement = getParticipantOfProcess(node as IProcessRef);
+
+        reporter.report(participant.id, 'This process has no StartEvent');
+      }
+
+    }
+  }
+
+  return {
+    check: check,
+  };
+};
