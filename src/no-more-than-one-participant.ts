18f95c39bec5c71fdc7422e965da003443959531
Vyperus <bastian.schnittger@5minds.de>
2019-02-20 16:32:25 +0100

    :sparkles: Add No More Than Two Participants Rule

---
+++ src/no-more-than-one-participant.ts
@@ -0,0 +1,33 @@
+import {IModdleElement} from '@process-engine/bpmn-elements_contracts';
+
+import * as lintUtils from 'bpmnlint-utils';
+import {BpmnLintReporter} from './contracts/bpmn-lint-reporter';
+
+/**
+ * Rule that reports conditional start events.
+ *
+ * Conditional start events are currently not supported by the Process Engine.
+ */
+module.exports = (): any => {
+
+  function check(node: IModdleElement, reporter: BpmnLintReporter): void {
+
+    const maxParticipantCount: number = 2;
+    let participantCount: number = 0;
+
+    const nodeIsParticipant: boolean = lintUtils.is(node, 'bpmn:Participant');
+    if (nodeIsParticipant) {
+      participantCount = participantCount + 1;
+
+      const tooManyPaticipantsPresent: boolean = participantCount >= maxParticipantCount;
+      if (tooManyPaticipantsPresent) {
+        reporter.report(node.id, 'Two or more Participants are currently not supported');
+      }
+
+    }
+  }
+
+  return {
+    check: check,
+  };
+};
