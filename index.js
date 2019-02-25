20bc8b3d06069057876c4e17ef018810a567439b
Vyperus <bastian.schnittger@5minds.de>
2019-02-25 15:18:03 +0100

    :sparkles: Add no Duplicate Ids Rule

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ module.exports = {
       rules: {
         'process-engine/no-more-than-one-participant': 'error',
         'process-engine/start-event-required': 'error',
-        'process-engine/end-event-required': 'error'
+        'process-engine/end-event-required': 'error',
+        'process-engine/no-duplicate-ids': 'error',
       }
     },
   }
