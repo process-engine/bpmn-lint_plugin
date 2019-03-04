5052ae1497b060b0dd9d4dc955c7c825df112ab8
Vyperus <bastian.schnittger@5minds.de>
2019-03-04 15:58:39 +0100

    :recycle: Simplify No Undefined Error Event Rule

--- src/no-undefined-error-event.ts
+++ src/no-undefined-error-event.ts
@@ -13,10 +13,7 @@ module.exports = (): any => {
   function check(node: IModdleElement, reporter: BpmnLintReporter): void {
 
     const nodeIsEvent: boolean = lintUtils.is(node, 'bpmn:BoundaryEvent')
-                              || lintUtils.is(node, 'bpmn:EndEvent')
-                              || lintUtils.is(node, 'bpmn:StartEvent')
-                              || lintUtils.is(node, 'bpmn:IntermediateThrowEvent')
-                              || lintUtils.is(node, 'bpmn:IntermediateCatchEvent');
+                              || lintUtils.is(node, 'bpmn:EndEvent');
 
     if (nodeIsEvent) {
       const eventElement: IEventElement = node as IEventElement;
