6671fedd47d2549c4ae1818362b943aa79d3baa3
robpal94 <robin.palkovits@5minds.de>
2019-03-07 16:00:25 +0100

    :sparkles: Add Validation for the TimerEventDefinitions

---
+++ src/invalid-timer-definition.ts
@@ -0,0 +1,89 @@
+import {IEventDefinition, IEventElement, IModdleElement, ITimerEventDefinition} from '@process-engine/bpmn-elements_contracts';
+import * as lintUtils from 'bpmnlint-utils';
+import {BpmnLintReporter} from './contracts/bpmn-lint-reporter';
+
+/**
+ * Rule which reports, if a defined TimerEvent contains a valid
+ * TimerEventDefinition.
+ *
+ * The TimerEventDefinition has to be in the ISO8601 Syntax.
+ */
+module.exports = (): any => {
+
+  const handledEventList: Array<string> = [
+    'bpmn:StartEvent',
+    'bpmn:IntermediateCatchEvent',
+    'bpmn:BoundaryEvent',
+  ];
+
+  function validateTimerEventDefinition(timerEventDefinition: ITimerEventDefinition, rootNodeId: string, reporter: BpmnLintReporter): void {
+    const timerIsDate: boolean = timerEventDefinition.timeDate !== undefined;
+    const timerIsDuration: boolean = timerEventDefinition.timeDuration !== undefined;
+    const timerIsCycle: boolean = timerEventDefinition.timeCycle !== undefined;
+
+    if (timerIsDate) {
+
+      /**
+       * Stolen from: https://stackoverflow.com/a/14322189
+       */
+      /*tslint:disable-next-line:max-line-length*/
+      const iso8601DateRegex: RegExp = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
+      const dateIsInvalid: boolean = !iso8601DateRegex.test(timerEventDefinition.timeDate.body);
+
+      if (dateIsInvalid) {
+        reporter.report(rootNodeId, 'Date is not in ISO8601 Syntax.');
+      }
+
+    } else if (timerIsDuration) {
+      /**
+       * Taken from: https://stackoverflow.com/a/32045167
+       */
+      /*tslint:disable-next-line:max-line-length*/
+      const durationRegex: RegExp = /^P(?!$)(\d+(?:\.\d+)?Y)?(\d+(?:\.\d+)?M)?(\d+(?:\.\d+)?W)?(\d+(?:\.\d+)?D)?(T(?=\d)(\d+(?:\.\d+)?H)?(\d+(?:\.\d+)?M)?(\d+(?:\.\d+)?S)?)?$/gm;
+      const durationIsInvalid: boolean = !durationRegex.test(timerEventDefinition.timeDuration.body);
+
+      if (durationIsInvalid) {
+        console.log(timerEventDefinition.timeDuration.body);
+        reporter.report(rootNodeId, 'Duration is not in ISO8601 Syntax.');
+      }
+
+    } else if (timerIsCycle) {
+      /**
+       * TODO: As specified in a special use case, we need to check if the
+       * Process contains a normal start Events, if the cyclic definition
+       * was attached on a StartEvent.
+       */
+      reporter.report(rootNodeId, 'Cyclic Timer definitions are currently not supported.');
+    } else {
+      reporter.report(rootNodeId, 'Unknown Timer Event Definition.');
+    }
+  }
+
+  function check(node: IModdleElement, reporter: BpmnLintReporter): void {
+    const nodeIsEvent: boolean = lintUtils.isAny(node, handledEventList);
+
+    if (nodeIsEvent) {
+      const eventElement: IEventElement = node as IEventElement;
+      const nodeContainsNoEventDefinitions: boolean =
+        eventElement.eventDefinitions === undefined
+        || eventElement.eventDefinitions.length === 0;
+
+      if (nodeContainsNoEventDefinitions) {
+        return;
+      }
+
+      for (const currentEventDefinition of eventElement.eventDefinitions) {
+        const currentEventDefIsTimer: boolean = lintUtils.is(currentEventDefinition, 'bpmn:TimerEventDefinition');
+
+        if (currentEventDefIsTimer) {
+          const currentTimerEventDefinition: ITimerEventDefinition = currentEventDefinition as ITimerEventDefinition;
+          validateTimerEventDefinition(currentTimerEventDefinition, node.id, reporter);
+        }
+      }
+    }
+  }
+
+  return {
+    check: check,
+  };
+};
