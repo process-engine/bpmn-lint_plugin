2d40c3ffaac1c65f1d31ed6ffe6967ca967cbd49
robpal94 <robin.palkovits@5minds.de>
2019-03-07 17:11:45 +0100

    :lipstick: Report a different ErrorMessage if the Input is Empty

--- src/invalid-timer-definition.ts
+++ src/invalid-timer-definition.ts
@@ -27,19 +27,36 @@ module.exports = (): any => {
     const timerIsCycle: boolean = timerEventDefinition.timeCycle !== undefined;
 
     if (timerIsDate) {
+      const timerDefinitionValue: string = timerEventDefinition.timeDate.body;
+      const valueIsEmpty: boolean = timerDefinitionValue.trim() === '';
+
+      if (valueIsEmpty) {
+        reporter.report(rootNodeId, 'Date is empty.');
+
+        return;
+      }
 
       /**
        * Stolen from: https://stackoverflow.com/a/14322189
        */
       /*tslint:disable-next-line:max-line-length*/
       const iso8601DateRegex: RegExp = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
-      const dateIsInvalid: boolean = !iso8601DateRegex.test(timerEventDefinition.timeDate.body);
+      const dateIsInvalid: boolean = !iso8601DateRegex.test(timerDefinitionValue);
 
       if (dateIsInvalid) {
         reporter.report(rootNodeId, 'Date is not in ISO8601 Syntax.');
       }
 
     } else if (timerIsDuration) {
+      const timerDefinitionValue: string = timerEventDefinition.timeDuration.body;
+      const valueIsEmpty: boolean = timerDefinitionValue.trim() === '';
+
+      if (valueIsEmpty) {
+        reporter.report(rootNodeId, 'Duration is empty.');
+
+        return;
+      }
+
       /**
        * Taken from: https://stackoverflow.com/a/32045167
        */
