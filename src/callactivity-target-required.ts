507fd446d487281832686b8600c787dcd6fd5a1d
robpal94 <robin.palkovits@5minds.de>
2019-03-14 14:26:13 +0100

    :sparkles: Add a Rule to Check if the Called Element is set

---
+++ src/callactivity-target-required.ts
@@ -0,0 +1,27 @@
+import {ICallActivityElement, IDefinitions, IModdleElement} from '@process-engine/bpmn-elements_contracts';
+
+import * as lintUtils from 'bpmnlint-utils';
+import {BpmnLintReporter} from './contracts/bpmn-lint-reporter';
+
+/**
+ * Rule that checks, if a CallActivity contains a target.
+ */
+module.exports = (): any => {
+
+  function check(node: IModdleElement, reporter: BpmnLintReporter): void {
+    const currentNodeIsCallActivity: boolean = lintUtils.is(node, 'bpmn:CallActivity');
+
+    if (currentNodeIsCallActivity) {
+      const currentCallActivity: ICallActivityElement = node as ICallActivityElement;
+      const calledElementNotAssigned: boolean = currentCallActivity.calledElement === null;
+
+      if (calledElementNotAssigned) {
+        reporter.report(currentCallActivity.id, 'No CalledElement assigned');
+      }
+    }
+  }
+
+  return {
+    check: check,
+  };
+};
